129d3024b63f7a9bf0b236fdbe265bebf07d701f
Carl Wieland <carlwieland@me.com>
2012-10-26 17:58:38 -0600

    Initial Commit

---
+++ Bloom/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  Bloom
+//
+//  Created by Carl & Hannah Wieland on 10/26/12.
+//  Copyright (c) 2012 balanceoni. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ Bloom/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Bloom
+//
+//  Created by Carl & Hannah Wieland on 10/26/12.
+//  Copyright (c) 2012 balanceoni. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
